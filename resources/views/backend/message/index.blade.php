884524e634746ea0be8a612abad90d57a321ab05
werlic <agussulaeman453@yahoo.co.id>
2020-08-09 06:49:42 +0700

    add message page and add first layout for message

---
+++ resources/views/backend/message/index.blade.php
@@ -0,0 +1,24 @@
+@extends('layouts.app')
+
+@section('title', 'Message')
+
+@section('content')
+<div class="container">
+	<div class="row">
+		<div class="col-12">
+			<div class="card bg-default p-0">
+				<div class="card-header">
+					<h4>Message</h4>
+				</div>
+				<div class="card-body">
+					
+				</div>
+				<div class="card-footer">
+					
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
+@endsection
