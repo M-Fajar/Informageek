8541879373a0beb9b65b9aeaf8c514097d58c35d
RidhaGinanjar <ridhaginanjar7@gmail.com>
2020-08-09 15:57:52 +0700

    Code User Management Edit

--- resources/views/backend/admin/user/index.blade.php
+++ resources/views/backend/admin/user/index.blade.php
@@ -4,7 +4,12 @@
     <div class="col-md-12">
         <div class="card">
             <div class="card-header">
-                <h4>User Management</h4>
+                <h4>User Management</h4> <br>
+                @if (session('status'))
+                    <div class="alert alert-success" role= "alert">
+                        {{session('status') }}
+                    </div>
+                @endif
                 <div class="card-header-action">
                 </div>
             </div>
@@ -14,26 +19,38 @@
                         <thead class="thead-light">
                             <tr>
                                 <th>No</th>
-                                <th>Nama User</th>
+                                <th>Nama</th>
                                 <th>Email</th>
+                                <th>User Name</th>
                                 <th>Tipe User</th>
                                 <th width="10%">Aksi</th>
+                                <th width="10%">Hapus</th>
                             </tr>
                         </thead>
                         <tbody>
-                           @foreach ( $users as $item)
+                        <?php $no = 0;?>
+                           @foreach ( $users->sortBy('id') as $item)
+                        <?php $no++ ;?> 
                             <tr>
-                                <td> </td>
-                                <td>{{ $item->name}} </td>
-                                <td>{{$item->email}}
-                                </td>
+                                <td>{{$no}}</td>
+                                <td>{{$item->name}} </td>
+                                <td>{{$item->email}}</td>
+                                <td>{{$item->username}}</td>
                                 <td> 
                                     {{$item->getRoleNames()[0]}}
                                 </td>
+                                @if($item->getRoleNames()[0] != 'admin')
                                 <td>
-                                    <a href=""
-                                        class="btn btn-warning">Detail</a>
+                                    <a href="/admin/userupdate/{{ $item->id }}" class="btn btn-warning">EDIT</a>
                                 </td>
+                                <td>
+                                    <form action="/admin/userdelete/{{ $item->id }}" method = "post" onsubmit= "return confirm('Yakin menghapus data tersebut ?')">
+                                        {{ csrf_field() }}
+                                        {{ method_field('DELETE') }}
+                                        <button href="#" class="btn btn-danger">DELETE</button>
+                                    </form>
+                                </td>
+                                @endif
                             </tr>
                             @endforeach
                         </tbody>
