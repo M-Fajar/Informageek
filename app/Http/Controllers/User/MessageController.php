f891c3069c80a0bdbdd610a3a2a2fecd31674b02
werlic <agussulaeman453@yahoo.co.id>
2020-09-10 12:38:08 +0700

    Add Model Message and Room; set Relationship model User, Room, Message; Set Controller Message

--- app/Http/Controllers/User/MessageController.php
+++ app/Http/Controllers/User/MessageController.php
@@ -4,6 +4,10 @@ namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Events\MessageSent;
+use App\Message;
+use App\Room;
 
 class MessageController extends Controller
 {
@@ -11,5 +15,46 @@ class MessageController extends Controller
     public function index()
     {
     	return view('backend.message.index');
+    }
+
+    public function fetchMessage($room)
+    {
+        $id_user = Auth::id();
+        $message = Message::with(['user' => function($q) 
+        {
+            $q->select('id','name');
+        }])->where('room_id', $room)->get();
+        $room = Room::find($room);
+        if (isset($room->name)) {
+            $user = $room->user()->select('name')->get();
+        } else {
+            $user = $room->user()->select('name')->where('users.id', '!=', $id_user)->get();
+        }
+        return ['user' => $user, 'chat' => $message];
+    }
+
+    public function fetchRoom()
+    {
+        $id_user = Auth::id();
+        $room_list = Room::with(['message' => function ($m)
+        {
+            $m->orderBy('id', 'desc')->first();
+        }, 'user' => function ($u) use ($id_user)
+        {
+            $u->select('users.id', 'users.name')->where('users.id', '!=', $id_user);
+        }])->get();
+        return $room_list;
+    }
+
+    public function sendMessage(Request $request)
+    {
+        $message = auth()->user()->message()->create([
+            'message' => $request->message,
+            'room_id' => $request->room
+        ]);
+
+        broadcast(new MessageSent($message->load('user')))->toOthers();
+
+        return ['status' => 'Success!!'];
     }
 }
--- app/User.php
+++ app/User.php
@@ -39,4 +39,14 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function message()
+    {
+        return $this->hasMany(Message::class);
+    }
+
+    public function room()
+    {
+        return $this->belongsToMany(Room::class, 'room_user');
+    }
 }
