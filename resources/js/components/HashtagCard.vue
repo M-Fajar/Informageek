f8acf534b197fcee5efe4bd80b5f6ebfcdb6520f
Ardnh <ardn.h79@gmail.com>
2020-11-10 13:37:09 +0700

    tambah hashtag

---
+++ resources/js/components/HashtagCard.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <div v-for="index in 5" :key="index">
+            <li class="media my-5 justify-content-between border-bottom">
+                <img src="/media/frontend/sugiono.png" alt="kakek lejen" class="img-fluid">
+                <div class="media-object mx-3 text-left flex-fill">
+                    <h2>#AnnisaFitri</h2>
+                    <p>250k</p>
+                </div>
+            </li>
+        </div>
+    </div>
+</template>
--- resources/js/pages/Search.vue
+++ resources/js/pages/Search.vue
@@ -1,43 +1,61 @@
 <template>
-  <div class="container-fluid">
+<div class="container-fluid">
     <div class="row mt-2">
-      <div class="col-md-3 p-3">
-        <div class="card border-0 shadow-sm">
-          <div class="card-body">
-            <div class="media">
-              <i class="fas fa-filter fa-2x mr-3 text-secondary"></i>
-              <div class="media-body">
-                <h2>Filter</h2>
-                <ul class="list-unstyled">
-                  <li><a href="" class="card-link text-danger h5">Akun</a></li>
-                  <li><a href="" class="card-link text-secondary h5">Filter</a></li>
-                  <li><a href="" class="card-link text-secondary h5">Hashtag</a></li>
-                </ul>
-              </div>
+        <div class="col-md-3 p-3">
+            <div class="card border-0 shadow-sm">
+                <div class="card-body">
+                    <div class="media">
+                        <i class="fas fa-filter fa-2x mr-3 text-secondary"></i>
+                        <div class="media-body">
+                            <h2>Filter</h2>
+                            <ul class="list-unstyled">
+                                <li><a href="" class="card-link text-secondary h5" :class="{ 'text-danger' : ispersonActive }" @click.prevent="personActive()">Akun</a></li>
+                                <li><a href="" class="card-link text-secondary h5" :class="{ 'text-danger' : ishashtagActive }" @click.prevent="hashtagActive()">Hashtag</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
             </div>
-          </div>
         </div>
-      </div>
-      <div class="col-md-6 p-3">
-        <div class="card border-0 shadow-sm rounded">
-          <div class="card-body p-5">
-            <h1 class="card-tile">Hasil Pencarian: <small>Annisa</small></h1>
-            <ul class="list-unstyled">
-              <PersonCard v-for="index in 5" :key="index"/>
-            </ul>
-          </div>
+        <div class="col-md-6 p-3">
+            <div class="card border-0 shadow-sm rounded">
+                <div class="card-body p-5">
+                    <h1 class="card-tile">Hasil Pencarian: <small>Annisa</small></h1>
+                    <ul class="list-unstyled">
+                        <PersonCard v-for="index in 5" :key="index" v-show="ispersonActive"/>
+                        <HashtagCard v-show="ishashtagActive"/>
+                    </ul>
+                </div>
+            </div>
         </div>
-      </div>
     </div>
-  </div>
+</div>
 </template>
 
 <script>
 import PersonCard from '../components/PersonCard';
+import HashtagCard from '../components/HashtagCard'
 export default {
-  components: {
-    PersonCard,
-  }
+    data() {
+        return{
+            ispersonActive: true,
+            ishashtagActive: false
+        }
+    },
+    components: {
+        PersonCard,
+        HashtagCard
+    },
+    methods: {
+        personActive(){
+            this.ispersonActive = true
+            this.ishashtagActive = false
+        },
+        hashtagActive(){
+            this.ishashtagActive = true
+            this.ispersonActive = false
+        }
+    }
 }
 </script>
 
